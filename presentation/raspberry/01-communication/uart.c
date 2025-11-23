cd585a56755b05c6f49a4993dd28a5156a736e11
Andreas Wögerbauer <andreas.woegerbauer@htl-wels.at>
2025-11-23 18:14:35 +0100

    added raspi uart

---
+++ presentation/raspberry/01-communication/uart.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <pigpio.h>
+
+int main(int argc, char *argv[])
+{
+    if (gpioInitialise() < 0)
+    {
+        fprintf(stderr, "pigpio initialization failed\n");
+        return 1;
+    }
+
+    // Open UART on /dev/serial0 with baud rate 115200
+    int handle = serOpen("/dev/ttyAMA0", 9600, 0);
+    if (handle < 0)
+    {
+        fprintf(stderr, "Failed to open UART\n");
+        gpioTerminate();
+        return 1;
+    }
+
+    // Example: write single characters
+    const char *message = "12340";
+    for (int i = 0; message[i] != '\0'; i++)
+    {
+        serWriteByte(handle, message[i]); // send one character
+        gpioDelay(500000); // optional small delay (1 ms)
+    }
+
+    // Close UART
+    serClose(handle);
+    gpioTerminate();
+    return 0;
+}
